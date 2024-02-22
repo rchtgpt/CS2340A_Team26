43e804fb58670ef75cc4dc00eb25a51c8538c05e
Rachit Gupta <guptarachit2004@gmail.com>
2024-02-21 23:42:54 -0500

    adds bottom nav bar & placeholder fragment text

--- app/src/main/java/com/example/greenplate/views/MainActivity.java
+++ app/src/main/java/com/example/greenplate/views/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.greenplate.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.greenplate.R;
+import com.example.greenplate.views.mainFragments.IngredientFragment;
+import com.example.greenplate.views.mainFragments.InputMealFragment;
+import com.example.greenplate.views.mainFragments.RecipeFragment;
+import com.example.greenplate.views.mainFragments.ShoppingListFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        replaceFragment(new InputMealFragment());
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.inputMeal) {
+                replaceFragment(new InputMealFragment());
+            } else if (item.getItemId() == R.id.recipe) {
+                replaceFragment(new RecipeFragment());
+            }
+            else if (item.getItemId() == R.id.ingredients) {
+                replaceFragment(new IngredientFragment());
+            } else {
+                replaceFragment(new ShoppingListFragment());
+            }
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment, fragment);
+        fragmentTransaction.commit();
     }
 }
