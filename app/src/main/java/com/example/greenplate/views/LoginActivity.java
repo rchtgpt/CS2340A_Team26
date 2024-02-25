ed59dae36b7b05302f61e73189e9cceed20d2bdb
Unnathi Kumar <unnathiutpal6@gmail.com>
2024-02-24 21:34:01 -0500

    adds firebase email authentication and database

--- app/src/main/java/com/example/greenplate/views/LoginActivity.java
+++ app/src/main/java/com/example/greenplate/views/LoginActivity.java
@@ -2,15 +2,55 @@ package com.example.greenplate.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.greenplate.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private EditText editTextUsername;
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        editTextUsername = findViewById(R.id.editTextUsernameLogin);
+        editTextPassword = findViewById(R.id.editTextPasswordLogin);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        TextView signUpText = findViewById(R.id.textViewSignUpLink);
+        signUpText.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+
+        loginButton.setOnClickListener(v -> {
+            String username = String.valueOf(editTextUsername.getText());
+            String password = String.valueOf(editTextPassword.getText());
+
+            firebaseAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(this, task -> {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
     }
 }
--- app/src/main/java/com/example/greenplate/views/SignUpActivity.java
+++ app/src/main/java/com/example/greenplate/views/SignUpActivity.java
@@ -1,14 +1,72 @@
 package com.example.greenplate.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.greenplate.R;
+import com.example.greenplate.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private TextView editTextFirstName;
+    private TextView editTextLastName;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        TextView loginTextView = findViewById(R.id.textViewLoginLink);
+        loginTextView.setOnClickListener(v -> {
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        Button registerButton = findViewById(R.id.registerButton);
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+
+        registerButton.setOnClickListener(v -> {
+            String email = String.valueOf(editTextUsername.getText());
+            String password = String.valueOf(editTextPassword.getText());
+            String firstName = String.valueOf(editTextFirstName.getText());
+            String lastName = String.valueOf(editTextLastName.getText());
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            User user = new User(firstName, lastName, email);
+                            databaseReference.child("users")
+                                    .child(firebaseAuth.getCurrentUser().getUid()).setValue(user);
+
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Failed to create user", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
     }
 }
