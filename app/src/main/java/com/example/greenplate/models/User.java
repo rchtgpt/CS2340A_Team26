b71861a9bd5b823cc89f2ae80369e4784edbadc2
adityakabu24 <akabu3@gatech.edu>
2024-03-11 17:16:43 -0400

    Reinitialized weight, height and gender attributes which were over-written in my previous commit

--- app/src/main/java/com/example/greenplate/models/User.java
+++ app/src/main/java/com/example/greenplate/models/User.java
@@ -8,12 +8,18 @@ public class User {
     private String lastName;
     private String email;
     private Map<String, Integer> meals;
+    private String height;
+    private String weight;
+    private String gender;
 
     public User(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.meals = new HashMap<>();
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
     }
 
     // Getter for firstName
@@ -57,4 +63,16 @@ public class User {
     public void addMeal(String mealName, Integer calories) {
         this.meals.put(mealName, calories);
     }
+
+    public String getHeight() {return height;}
+
+    public void setHeight(String height) {this.height = height;}
+
+    public String getWeight() {return weight;}
+
+    public void setWeight(String weight) {this.weight = weight;}
+
+    public String getGender() {return gender;}
+
+    public void setGender(String gender) {this.gender = gender;}
 }
